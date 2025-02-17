f378907dd3a6f7d9ed79737bbc21b74782510ccf
CrawlingTug <83224884+CrawlingTug@users.noreply.github.com>
2025-02-17 19:44:57 +0100

    changes to landingpage

--- src/components/LandingPage/index.tsx
+++ src/components/LandingPage/index.tsx
@@ -6,17 +6,51 @@ export default function HomepageMinimal(): JSX.Element {
   return (
     <div className={clsx("container", styles.centerContent)}>
       <div className="text--center">
+        <h1>Willkommen auf der Citybuild Wiki</h1>
         <img
           src={require("./gommeskin.png").default}
           alt="Citybuld Wiki Gomme"
-          className={styles.image}
+          className={styles.imageSmall}
           onClick={() => (window.location.href = "/docs/category/lobby")}
           style={{ cursor: "pointer" }}
         />
       </div>
       <div className="text--center">
-        <h1>CityBuild Wiki</h1>
-        <p>Deine Guide für CityBuild.</p>
+        <h1>Erste Schritte</h1>
+      </div>
+      <div className={styles.buttonGrid}>
+        <button
+          className={styles.button}
+          onClick={() => (window.location.href = "/docs/placeholder")}
+        >
+          Neu hier?
+        </button>
+        <button
+          className={styles.button}
+          onClick={() =>
+            (window.location.href =
+              "/docs/category/informtionen-zur-seitenerstellung")
+          }
+        >
+          Seiten Erstellen
+        </button>
+      </div>
+      <div className="text--center">
+        <h1>Wichtige Guides</h1>
+        <div className={styles.buttonGrid}>
+          <button
+            className={styles.button}
+            onClick={() => (window.location.href = "/docs/placeholder")}
+          >
+            Artikel 1
+          </button>
+          <button
+            className={styles.button}
+            onClick={() => (window.location.href = "/docs/placeholder")}
+          >
+            Artikel 2
+          </button>
+        </div>
       </div>
     </div>
   );
